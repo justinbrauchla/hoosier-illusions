7bfc207f68436c026f1def3cb997793b7c16ec69
Justin Brauchla <justinbrauchla@Justins-MacBook-Air.local>
2025-11-23 21:33:13 -0500

    fix: add crossOrigin to kill CORB and show owl

--- App.tsx
+++ App.tsx
@@ -511,6 +511,7 @@ const App: React.FC = () => {
                                 src={effectiveImageSrc}
                                 alt="Cover"
                                 className="w-full h-full object-cover"
+                                crossOrigin="anonymous"
                                 onError={(e) => {
                                   e.currentTarget.src = 'https://storage.googleapis.com/hoosierillusionsimages/OwlWhiteTransparent.png';
                                 }}
